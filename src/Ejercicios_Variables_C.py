e3d3692d9e2ba21b13ab0cbb9ba7d52771548483
CAMILA CORTES <camilacortes775@gmail.com>
2026-09-23 10:50:21 -0500

    Agrega ejercicio de area de triangulo

---
+++ src/Ejercicios_Variables_C.py
@@ -0,0 +1,9 @@
+# EJERCICIO 1: Área de un triángulo
+
+base = float(input("Ingrese la base del triángulo en cm: "))
+altura = float(input("Ingrese la altura del triángulo en cm: "))
+
+area = (base * altura) / 2
+
+print("El área del triángulo es:", area,
+ "cm²")
